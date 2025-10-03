786d6ca63565f6b04781756e7f5684c516a9cb62
Ava DeCristofaro <avadecristofaro@Mac.attlocal.net>
2025-10-03 13:03:34 -0700

    simple start screen button

---
+++ Assets/Scripts/StartScreen.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+    
+}
