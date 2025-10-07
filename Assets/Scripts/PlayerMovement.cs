7b82396f9dd30b7d5dda6da5deb99d5fefcae8f1
mjmpmanny <chapmanny2785@gmail.com>
2025-10-06 21:37:07 -0700

    Added camera movement with confinement, and jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,12 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     private float horizontalMovement;
+
+    public float jumpPower = 7f;
+
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize = new Vector2(.5f, .05f);
+    public LayerMask groundLayer;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,5 +29,34 @@ public class PlayerMovement : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (IsGrounded())
+        {
+            if (context.performed)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+            } else if (context.canceled)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+            }
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0f, groundLayer))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
     }
 }
