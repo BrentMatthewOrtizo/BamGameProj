e5b5afadb691b2f2c44f03de4d0a0bf87e847c30
Ava DeCristofaro <avadecristofaro@Avas-MacBook-Air-3.local>
2025-10-08 20:58:04 -0700

    inventory backend for emblems and food

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,19 +1,41 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class InventoryManager : MonoBehaviour
 {
-    public int itemCount;
-
-    public bool PickupItem(GameObject item)
+    public List<ItemClass> items = new List<ItemClass>();
+    public ItemClass itemToAdd;
+    public ItemClass itemToRemove; //after accessing inventory, select item and set it to this in order to remove it
+    public void Start()
     {
-        switch (item.tag)
-        {
-            case Constants.TAG_FOOD:
-                itemCount++;
-                return true;
-            default:
-                return false;
-        }
-        
+        Add(itemToAdd);//would put this when u collide with item or press E
+        Remove(itemToRemove); //put here just to test
     }
+    
+    public void Add(ItemClass item)
+    {
+        items.Add(item);
+    }
+
+    public void Remove(ItemClass item)
+    {
+        items.Remove(item);
+    }
+    
+    
+    // public int itemCount;
+    //
+    // public bool PickupItem(GameObject item)
+    // {
+    //     switch (item.tag)
+    //     {
+    //         case Constants.TAG_FOOD:
+    //             itemCount++;
+    //             return true;
+    //         default:
+    //             return false;
+    //     }
+    //     
+    // }
 }
