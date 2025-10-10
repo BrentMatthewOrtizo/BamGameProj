8d2c32c9d62d20807e4f003f7d5255a7688c9bb4
mjmpmanny <chapmanny2785@gmail.com>
2025-10-09 18:29:51 -0700

    nothing

--- Assets/Scripts/ReturnButtonHandler.cs
+++ Assets/Scripts/ReturnButtonHandler.cs
@@ -3,7 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class ReturnButtonHandler : MonoBehaviour
 {
-    // Called by the UI Button's OnClick event
     public void OnClick()
     {
         if (!string.IsNullOrEmpty(WarpManager.previousSceneName))
